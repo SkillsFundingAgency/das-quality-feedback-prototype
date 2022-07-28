5a8b7df713236c02009265607bc4c6448e02fa1d
victorivandfe <77013703+victorivandfe@users.noreply.github.com>
2022-07-28 11:56:04 +0100

    Built exit survey prototype

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,23 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+
+// Re-direct from multiple  radios 
+router.post('/exit/exit2', function (req, res) {
+
+    let chooseoption = req.session.data['chooseoption']
+
+    console.log(req.session.data['chooseoption'])
+
+    //Option 1
+    if (chooseoption === 'yes') {
+        res.redirect('/exit/exit3')
+    }
+    //Option 2
+    else if (chooseoption === 'no') {
+        res.redirect('/exit/exit2b')
+    }
+
+    //end
+})
