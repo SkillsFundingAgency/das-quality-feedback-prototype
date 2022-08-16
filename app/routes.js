7fcaa1ff653e860fdea99a7adb1f30e84f90b7ee
victorivandfe <77013703+victorivandfe@users.noreply.github.com>
2022-08-16 22:01:10 +0100

    Footer + header + minor content changes

--- app/routes.js
+++ app/routes.js
@@ -14,11 +14,11 @@ router.post('/exit/exit2a', function (req, res) {
     console.log(req.session.data['chooseoption'])
 
     //Option 1
-    if (chooseoption === 'Yes, I did not complete') {
+    if (chooseoption === 'I did not complete') {
         res.redirect('/exit/exit3')
     }
     //Option 2
-    else if (chooseoption === 'No') {
+    else if (chooseoption === 'I did complete') {
         res.redirect('/exit/exit2b')
     }
 
