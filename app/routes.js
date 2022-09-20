9385a406d248f2636f148a30b373e4ce395943c3
victorivandfe <77013703+victorivandfe@users.noreply.github.com>
2022-09-20 16:13:30 +0100

    Amended content

--- app/routes.js
+++ app/routes.js
@@ -24,3 +24,25 @@ router.post('/exit/exit2a', function (req, res) {
 
     //end
 })
+
+
+
+
+// Re-direct from multiple  radios 
+router.post('/exit2/exit2a', function (req, res) {
+
+    let chooseoption = req.session.data['chooseoption']
+
+    console.log(req.session.data['chooseoption'])
+
+    //Option 1
+    if (chooseoption === 'Yes, this is correct') {
+        res.redirect('/exit2/exit3')
+    }
+    //Option 2
+    else if (chooseoption === 'No, this is not correct') {
+        res.redirect('/exit2/exit2b')
+    }
+
+    //end
+})
